21e4c46c58b5ac3759d29928f87968741e2fa6b7
sxyx2008 <sxyx2008@163.com>
2012-10-31 16:49:36 +0800

    Signed-off-by: sxyx2008 <sxyx2008@163.com>

--- src/main/java/org/activiti/web/simple/webapp/controller/WorkFlowController.java
+++ src/main/java/org/activiti/web/simple/webapp/controller/WorkFlowController.java
@@ -1,8 +1,11 @@
 package org.activiti.web.simple.webapp.controller;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.activiti.engine.FormService;
 import org.activiti.engine.HistoryService;
@@ -11,11 +14,13 @@ import org.activiti.engine.ManagementService;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value="/workflow")
@@ -50,30 +55,22 @@ public class WorkFlowController {
 	
 	
 	
-	
-	
-	
 	/**
 	 * Spring MVC文件上传
 	 * @return
 	 * @throws Exception 
 	 */
 	@RequestMapping(value="/fileupload",method=RequestMethod.POST)
-	public String upload(@RequestParam("username")String username,@RequestParam("file")MultipartFile file){
+	public String upload(@RequestParam("username")String username,@RequestParam("file")MultipartFile file,HttpServletRequest request, HttpServletResponse response,RedirectAttributes redirectAttributes) throws Exception{
 		System.out.println(username);
-		String message="";
 		if(!file.isEmpty()){
-			try {
-				byte[] bytes = file.getBytes();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			/*String realPath = request.getSession().getServletContext().getRealPath("/upload");
+			byte[] bytes= file.getBytes();
+			String realPath = request.getSession().getServletContext().getRealPath("/upload");
 			File out=new File(realPath,file.getName());
 			FileUtils.writeByteArrayToFile(out, bytes);
-			message="文件上传成功!保存在:"+out.getPath();*/
+			redirectAttributes.addFlashAttribute("message", "文件上传成功!保存在:"+out.getPath());
 		}
-		return "redirect:main";
+		return "redirect:/main";
 	}
 	
 	
