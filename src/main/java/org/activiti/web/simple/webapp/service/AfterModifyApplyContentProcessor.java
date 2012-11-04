f57633cab58ac595980ab2c9253b8f52bd11ae47
Administrator <Administrator@lenovo>
2012-11-04 16:55:39 +0800

    Signed-off-by: Administrator <Administrator@lenovo>

---
+++ src/main/java/org/activiti/web/simple/webapp/service/AfterModifyApplyContentProcessor.java
@@ -0,0 +1,37 @@
+package org.activiti.web.simple.webapp.service;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.web.simple.webapp.model.Leave;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AfterModifyApplyContentProcessor implements TaskListener{
+
+	@Resource(name="leaveServiceImpl")
+	private LeaveService leaveService;
+	
+	@Autowired
+	RuntimeService runtimeService;
+	
+	public void notify(DelegateTask delegateTask) {
+		String processInstanceId = delegateTask.getProcessInstanceId();
+		ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+		Leave leave = leaveService.findById(processInstance.getBusinessKey());
+		leave.setLeaveType((String) delegateTask.getVariable("leaveType"));
+		leave.setStartTime((Date) delegateTask.getVariable("startTime"));
+		leave.setEndTime((Date) delegateTask.getVariable("endTime"));
+		leave.setReason((String) delegateTask.getVariable("reason"));
+		leaveService.save(leave);
+	}
+
+}
